1e4b70af39190e3a50a5b2cc8fffd0ff97eee4da
Giamo Lao <giamolao98@gmail.com>
2023-05-21 17:17:25 -0400

    Working on fixing game loop but good progress so far

--- game/routes.py
+++ game/routes.py
@@ -6,14 +6,20 @@ from . import app  # replace 'your_main_file' with the actual name of your main 
 def home():
     return render_template('home.html')
 
+#game.html route
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
 @app.route('/start', methods=['GET'])
 def start_game():
     game_state = initialize_game_state()
     situation = describe_situation(game_state)
-    return jsonfy({'game_state': game_state, 'situation': situation})
+    return jsonify({'game_state': game_state, 'situation': situation})
 
 @app.route('/action', methods=['POST'])
 def perform_action():
+    print("perform action endpoint hit")
     game_state = request.json['game_state']
     action = request.json['action']
     game_state = handle_action(game_state, action)
--- game/static/js/game.js
+++ game/static/js/game.js
@@ -1,10 +1,15 @@
+let gameState = null;  // add this line
+
 window.onload = startGame;
 
 function startGame() {
+    document.getElementById('loading').style.display = 'block';
     fetch('/start')
         .then(response => response.json())
         .then(data => {
             document.getElementById('situation').textContent = data.situation;
+            gameState = data.game_state;
+            document.getElementById('loading').style.display = 'none';
         });
 }
 
@@ -12,15 +17,28 @@ document.getElementById('submit').onclick = submitAction;
 
 function submitAction() {
     const action = document.getElementById('action').value;
+
+    document.getElementById('loading').style.display = 'block';
     fetch('/action', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ action: action })
+        body: JSON.stringify({ game_state: gameState, action: action })  // modify this line
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                console.error("Error status:", response.status);
+                return;
+            }
+            return response.json();
+        })
         .then(data => {
-            document.getElementById('situation').textContent = data.situation;
-        });
+            if (data) {
+                document.getElementById('situation').textContent = data.situation;
+                gameState = data.game_state;  // add this line
+            }
+            document.getElementById('loading').style.display = 'none';
+        })
+        .catch(error => console.error("Error:", error));
 }
